2a36232c993ba417e5fd96dc217d0f6395e04de9
isharab <sherijigarx@gmail.com>
2024-03-29 14:47:10 +0500

    changing the struct

--- neurons/validator.py
+++ neurons/validator.py
@@ -20,7 +20,9 @@
 import os
 import sys
 import asyncio
-
+import datetime as dt
+import wandb
+import bittensor as bt
 # Set the project root path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 # Set the 'AudioSubnet' directory path
@@ -34,15 +36,19 @@ from lib.globals import service_flags
 from classes.tts import TextToSpeechService 
 from classes.vc import VoiceCloningService
 from classes.ttm import MusicGenerationService
+from classes.aimodel import AIModelService
 
 
-class AIModelController:
+
+class AIModelController():
     def __init__(self):
+        self.aimodel = AIModelService()
         self.text_to_speech_service = TextToSpeechService()
         self.music_generation_service = MusicGenerationService()
         self.voice_cloning_service = VoiceCloningService()
         self.current_service = self.text_to_speech_service
         self.service = service_flags
+        self.check_and_update_wandb_run()
 
     async def run_services(self):
         while True:
@@ -55,6 +61,40 @@ class AIModelController:
             elif isinstance(self.current_service, VoiceCloningService) and self.service["VoiceCloningService"]:
                 await self.current_service.run_async()
                 self.current_service = self.text_to_speech_service
+
+    def check_and_update_wandb_run(self):
+        # Calculate the time difference between now and the last run start time
+        current_time = dt.datetime.now()
+        time_diff = current_time - self.last_run_start_time
+        # Check if 4 hours have passed since the last run start time
+        if time_diff.total_seconds() >= 60 * 60:  # 4 hours * 3600 seconds/hour
+            self.last_run_start_time = current_time  # Update the last run start time to now
+            if self.wandb_run:
+                wandb.finish()  # End the current run
+            self.new_wandb_run()  # Start a new run
+
+    def new_wandb_run(self):
+        now = dt.datetime.now()
+        run_id = now.strftime("%Y-%m-%d_%H-%M-%S")
+        name = f"Validator-{self.uid}-{run_id}"
+        commit = self.aimodel.get_git_commit_hash()
+        self.wandb_run = wandb.init(
+            name=name,
+            project="subnet16",
+            entity="testingforsubnet16",
+            config={
+                "uid": self.aimodel.uid,
+                "hotkey": self.aimodel.wallet.hotkey.ss58_address,
+                "run_name": run_id,
+                "type": "Validator",
+                "tao (stake)": self.aimodel.tao,
+                "commit": commit,
+            },
+            tags=self.aimodel.sys_info,
+            allow_val_change=True,
+            anonymous="allow",
+        )
+        bt.logging.debug(f"Started a new wandb run: {name}")
 
 if __name__ == "__main__":
     controller = AIModelController()
