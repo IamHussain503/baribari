d8ea23e651a18abe2e88de9ac8e10d48f5b35558
isharab <sherijigarx@gmail.com>
2024-03-29 22:17:30 +0500

    Update tts.py

--- classes/tts.py
+++ classes/tts.py
@@ -97,9 +97,6 @@ class TextToSpeechService(AIModelService):
                 self.process_responses(filtered_axons, responses, g_prompt)
 
                 if self.last_reset_weights_block + 50 < self.current_block:
-                    bt.logging.info(f"Checking for updates ")                    
-                    if self.config.auto_update == 'yes':
-                        lib.utils.try_update()
                     bt.logging.info(f"Resetting weights for validators and nodes without IPs")
                     self.last_reset_weights_block = self.current_block        
                     # set all nodes without ips set to 0
@@ -122,6 +119,9 @@ class TextToSpeechService(AIModelService):
             bt.logging.info(f"Current block is {self.current_block}")
             self.update_weights(self.scores)
             self.last_updated_block = self.current_block
+            bt.logging.info(f"Checking for github update.")                    
+            if self.config.auto_update == 'yes':
+                lib.utils.try_update()
         else:
             bt.logging.info(f"Updating weights. Last update was at block:  {self.last_updated_block}")
             bt.logging.info(f"Current block is: {self.current_block}")
