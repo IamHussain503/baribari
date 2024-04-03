36b55926ee44d0e66daa2ad0535de383f47ad4ed
isharab <sherijigarx@gmail.com>
2024-04-04 02:37:49 +0500

    Update miner.py

--- neurons/miner.py
+++ neurons/miner.py
@@ -89,7 +89,7 @@ def get_config():
     )
 
     # Adds override arguments for network and netuid.
-    parser.add_argument("--netuid", type=int, default=1, help="The chain subnet uid.")
+    parser.add_argument("--netuid", type=int, default=16, help="The chain subnet uid.")
     bt.subtensor.add_args(parser)
     bt.logging.add_args(parser)
     bt.wallet.add_args(parser)
